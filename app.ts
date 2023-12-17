cf6def8acfc06a7649581d054c40f7b41001ff8a
Whalter Duarte <dev@whalter.com.br>
2023-12-17 19:09:15 -0300

    update json

--- app.ts
+++ app.ts
@@ -34,6 +34,10 @@ app.use(cors());
    //Blog
  app.use('/blog',blog)
 
+//Para rotas não encotradas
+app.use((req: Request, res:Response)=>{
+  res.status(404).send('Pagina não encontrada')
+})
 
 
 app.listen(port, () => console.log(`Servidor rodando na porta : ${port}!`))
