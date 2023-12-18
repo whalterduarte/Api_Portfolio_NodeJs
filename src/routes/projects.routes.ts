a3bec6160f43eb4a587a830890dfd9f88e87f0ba
Whalter Duarte <dev@whalter.com.br>
2023-12-18 15:06:10 -0300

    AWS3 ADD

--- src/routes/projects.routes.ts
+++ src/routes/projects.routes.ts
@@ -1,41 +1,46 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
 import * as projects from '../controller/projects.controller'
-import multer from "multer"
-import { Auth } from '../middleware/auth'
+import multer, { Multer } from 'multer'
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+import multerS3 from 'multer-s3'
 
-//Multer
-const storage = multer.diskStorage({
-  destination: (req, file, cb)=>{
-    cb(null, './public/uploads')
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION || 'sa-east-1',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
   },
-  filename: (req, file, cb)=>{
-    let randomNamePhoto = Math.floor(Math.random()*9999999)
-    cb(null, file.fieldname+randomNamePhoto+Date.now()+'.jpg')
-  }
-})
-const upload = multer({
-  storage,
-  //Filter
-  fileFilter:(req, file, cb)=>{
-    const allowed: string[] =  ['image/jpg','image/jpeg', 'image/png']
-    if(allowed.includes(file.mimetype)){
-      cb(null, true)
-    }
-     else{ cb(null, false)
-    return
-  }
-    
-  }
 })
 
+const upload: Multer = multer({
+  storage: multerS3({
+    s3: s3Client,
+    bucket: process.env.BUCKET_NAME || '',
+    acl: 'public-read',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: (req: Request, file, cb) => {
+      console.log('File Object:', file); // Adicione esta linha para imprimir o objeto file
+      let randomNamePhoto = Math.floor(Math.random() * 9999999);
+      const originalname = file.originalname || 'default';
+      cb(null, 'uploads/' + originalname + randomNamePhoto + Date.now() + '.jpg');
+    },
+  }),
+  fileFilter: (req: Request, file: Express.Multer.File, cb) => {
+    const allowed: string[] = ['image/jpg', 'image/jpeg', 'image/png']
+    if (allowed.includes(file.mimetype)) {
+      cb(null, true)
+    } else {
+      cb(new Error('Invalid file type'))
+    }
+  }
+})
 
 const router = Router()
 
-//Projects
-router.get('/', projects.getProjects )
+// Projects
+router.get('/', projects.getProjects)
 router.get('/:slug', projects.descProject)
 
-router.post('/add',  upload.single('photo'), projects.addProject)
+router.post('/add', upload.single('photo'), projects.addProject)
 
-export default router;
-
+export default router
